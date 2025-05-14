0705494861582ad87336c279459021ede8714019
VeerShahCodes <shahveer1213@gmail.com>
2025-05-13 18:01:05 -0700

    Created grid creation and cell hoverring

--- PathfindingVisualizer/Game1.cs
+++ PathfindingVisualizer/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGame.Extended;
@@ -8,6 +9,10 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager graphics;
     private SpriteBatch spriteBatch;
+    private int gridSize;
+    private int screenWidth;
+    private int screenHeight;
+
 
     public Game1()
     {
@@ -19,6 +24,13 @@ public class Game1 : Game
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
+        gridSize = 20;
+
+        screenHeight = 500;
+        screenWidth = 500;
+        graphics.PreferredBackBufferHeight = screenHeight;
+        graphics.PreferredBackBufferWidth = screenWidth;
+        graphics.ApplyChanges();
 
         base.Initialize();
     }
@@ -42,11 +54,30 @@ public class Game1 : Game
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-        spriteBatch.Begin();
-        
-        // TODO: Add your drawing code here
 
+        GraphicsDevice.Clear(Color.White);
+        spriteBatch.Begin();
+        var mouseState = Mouse.GetState();
+        Point mousePosition = new Point(mouseState.X, mouseState.Y);
+        int hoverX = (mousePosition.X / gridSize) * gridSize;
+        int hoverY = (mousePosition.Y / gridSize) * gridSize;
+        Rectangle hoveredCell = new Rectangle(hoverX, hoverY, gridSize, gridSize);
+        for(int rows = 0; rows < screenWidth; rows+=gridSize) {
+            for(int cols = 0; cols < screenHeight; cols+=gridSize) {
+                Rectangle rect = new Rectangle(rows, cols, gridSize, gridSize);
+                Color color = Color.LightGray;
+                if(rect == hoveredCell) { //if is hovered change to black
+                    color = Color.LightGreen;
+                    spriteBatch.FillRectangle(rect, color);
+                }
+                else {
+                    spriteBatch.DrawRectangle(rect, color);
+                }
+                
+            }
+        }
         base.Draw(gameTime);
+        spriteBatch.End();
+
     }
 }
